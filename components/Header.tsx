030ccf720cdf55f5d163a08119a993309bcbe2cf
Syed Jawad <jawadsyed406@gmail.com>
2023-12-17 04:25:41 +0300

    Header Left Caret

---
+++ components/Header.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+import { RxCaretLeft } from "react-icons/rx";
+
+interface HeaderProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+const Header:React.FC<HeaderProps> = ({
+  children,
+  className
+}) => {
+   const router = useRouter();
+   
+   const handleLogout = () => {
+    //Handle logout in the future
+   }
+
+    return (
+        <div
+          className={twMerge(`
+            h-fit 
+            bg-gradient-to-b
+            from-emerald-800
+            p-6
+          `,
+            className
+          )}
+        >
+          <div className="
+          w-full
+          mb-4
+          flex
+          items-center
+          justify-between
+          ">
+           <div className="
+             hidden
+             md:flex
+             gap-x-2
+             items-center
+           ">
+              <button>
+                <RxCaretLeft size={35} />
+              </button>
+           </div>
+          </div>
+        </div>
+    );
+}
+export default Header;
