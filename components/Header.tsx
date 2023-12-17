5c7d109a20c01e5ff31aa419ea30a2b0a79edbd3
Syed Jawad <jawadsyed406@gmail.com>
2023-12-17 04:46:07 +0300

    Header Carets

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,8 @@
 
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
-import { RxCaretLeft } from "react-icons/rx";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -43,9 +44,39 @@ const Header:React.FC<HeaderProps> = ({
              gap-x-2
              items-center
            ">
-              <button>
-                <RxCaretLeft size={35} />
+              <button
+                onClick={() => router.back()}
+                className="
+                  rounded-full
+                  bg-black
+                  flex
+                  items-center
+                  justify-center
+                  hover:opacity-75
+                  transition
+                "
+              >
+                <RxCaretLeft className="text-white" size={35} />
               </button>
+              <button
+                onClick={() => router.forward()}
+                className="
+                  rounded-full
+                  bg-black
+                  flex
+                  items-center
+                  justify-center
+                  hover:opacity-75
+                  transition
+                "
+              >
+                <RxCaretRight className="text-white" size={35} />
+              </button>
+           </div>
+           <div className="flex md:hidden gap-x-2 items-center">
+             <button>
+                <HiHome />
+             </button>
            </div>
           </div>
         </div>
