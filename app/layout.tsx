1b73a8eed0e6e329701d4c121df646432e87339d
Syed Jawad <jawadsyed406@gmail.com>
2024-08-19 20:49:49 +0300

    Authentication modal and functionality

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from 'next'
+import { Figtree } from 'next/font/google'
+import './globals.css'
+
+import Sidebar from '@/components/Sidebar'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
+import ToasterProvider from '@/providers/ToasterProvider'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Spotify Clone',
+  description: 'Listen to sound!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ToasterProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+        <Sidebar>
+          {children}
+        </Sidebar>     
+        </UserProvider>
+        </SupabaseProvider>
+        </body>
+    </html>
+  )
+}
---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import AuthModal from "@/components/AuthModal";
+
+const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+useEffect(() => {
+    setIsMounted(true);
+}, []);
+
+if (!isMounted) {
+    return null;
+}
+
+return (
+  <>
+    <AuthModal />
+  </>
+);
+}
+
+export default ModalProvider;
